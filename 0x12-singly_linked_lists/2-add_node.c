db23421a65c17f146e7e2047623ee0359be0885d
Mohammed <125609592+simedaz@users.noreply.github.com>
2023-04-28 01:19:18 +0000

    Add files via upload

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+int _strlen_recursion(char *s);
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
+list_t *add_node(list_t **head, const char *str)
+{
+
+    list_t *new;
+
+    new = malloc(sizeof(list_t));
+
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+
+    new->str = strdup(str);
+
+    if (!new->str)
+    {
+        free(new);
+        return (NULL);
+    }
+	new->len = _strlen_recursion(new->str);
+	new->next = *head;
+	*head = new;
+    return (new);
+}
